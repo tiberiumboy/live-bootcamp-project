a64df309b1d1f5cc2f5cbef888a663ba4063bce1
MegaMind <8661186+jordanbejar@users.noreply.github.com>
2024-10-23 17:31:29 -0700

    [Bugfix]Add token ban check in verify token impl.

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,9 +1,20 @@
+use crate::app_state::AppState;
 use crate::routes::jwt::JWToken;
 use crate::utils::auth::validate_token;
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::{response::IntoResponse, Json};
 
-pub async fn verify_token(Json(jwt): Json<JWToken>) -> impl IntoResponse {
+pub async fn verify_token(
+    State(app): State<AppState>,
+    Json(jwt): Json<JWToken>,
+) -> impl IntoResponse {
+    let store = app.banned_token_store.clone();
+    let ban_list = store.read().await;
+    if ban_list.check_token(&jwt.token).await {
+        return StatusCode::UNAUTHORIZED.into_response();
+    }
+
     match validate_token(&jwt.token).await {
         Ok(_) => StatusCode::OK.into_response(),
         Err(_) => StatusCode::UNAUTHORIZED.into_response(),
